1f5ab2f5176fd4855068b210f6491b6ce1a92b72
Wilberforce <wmaggickk@gmail.com>
2024-02-15 14:39:08 +0000

    Added searching to user profile API

--- profiles_api/permissions.py
+++ profiles_api/permissions.py
@@ -7,6 +7,6 @@ class updateOwnProfile(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         """Check user is trying to edit their own profile"""
         if request.method in permissions.SAFE_METHODS:
-            return Truedd
+            return True
 
         return obj.id == request.user.id
