e7f5fdb940b448fc073cc3895ac0591bae2f5a09
Wilberforce <wmaggickk@gmail.com>
2024-02-15 14:03:12 +0000

    Added profile viewset

---
+++ profiles_api/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path,include
+from profiles_api import views
+
+from rest_framework.routers import DefaultRouter
+
+
+router = DefaultRouter()
+router.register('profile', views.UserProfileViewSet)
+
+
+urlpatterns = [
+    path('hello-view/', views.HelloApiView.as_view()),
+    path('', include(router.urls)),
+]
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
 
-# Create your views here.
+from profiles_api import serializers
+from profiles_api import models
+from profiles_api import permissions
+
+
+class HelloApiView(APIView):
+    """Test API View"""
+    serializer_class = serializers.HelloSerializer
+
+    def get(self, request, format=None):
+        """Returns a list of APIView features"""
+        an_apiview = [
+            'Uses HTTP methods as functions (get, post, patch, put, delete)'
+            'Is similar toa traditional Django View',
+            'Gives you the most control over your application logic',
+            'Is mapped manually to URLs',
+        ]
+
+        return Response({'message': 'Hello', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        """Create a hello message with our name"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Sup {name}'
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.updateOwnProfile,)
