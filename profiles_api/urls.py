43076681afb320d6c91ae1f99ba3dcfdcfecff0c
Wilberforce <wmaggickk@gmail.com>
2024-03-02 20:13:27 +0000

    no use

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -10,7 +10,6 @@ router.register('feed', views.UserProfileFeedViewSet)
 
 
 urlpatterns = [
-    path('hello-view/', views.HelloApiView.as_view()),
     path('login/', views.UserLoginApiView.as_view()),
     path('', include(router.urls)),
 ]
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -13,31 +13,31 @@ from profiles_api import models
 from profiles_api import permissions
 
 
-class HelloApiView(APIView):
-    """Test API View"""
-    serializer_class = serializers.HelloSerializer
+# class HelloApiView(APIView):
+#     """Test API View"""
+#     serializer_class = serializers.HelloSerializer
 
-    def get(self, request, format=None):
-        """Returns a list of APIView features"""
-        an_apiview = [
-            'Uses HTTP methods as functions (get, post, patch, put, delete)'
-            'Is similar toa traditional Django View',
-            'Gives you the most control over your application logic',
-            'Is mapped manually to URLs',
-        ]
+#     def get(self, request, format=None):
+#         """Returns a list of APIView features"""
+#         an_apiview = [
+#             'Uses HTTP methods as functions (get, post, patch, put, delete)'
+#             'Is similar to a traditional Django View',
+#             'Gives you the most control over your application logic',
+#             'Is mapped manually to URLs',
+#         ]
 
-        return Response({'message': 'Hello', 'an_apiview': an_apiview})
+#         return Response({'message': 'Hello', 'an_apiview': an_apiview})
 
-    def post(self, request):
-        """Create a hello message with our name"""
-        serializer = self.serializer_class(data=request.data)
+#     def post(self, request):
+#         """Create a hello message with our name"""
+#         serializer = self.serializer_class(data=request.data)
 
-        if serializer.is_valid():
-            name = serializer.validated_data.get('name')
-            message = f'Sup {name}'
-            return Response({'message': message})
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#         if serializer.is_valid():
+#             name = serializer.validated_data.get('name')
+#             message = f'Sup {name}'
+#             return Response({'message': message})
+#         else:
+#             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class UserProfileViewSet(viewsets.ModelViewSet):
